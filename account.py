652f58ee62a2dd6a18f4fd6e5da507f1b5b59056
lorrainekamanda <lorrainekamanda@gmail.com>
2020-04-18 02:03:16 +0300

    add method that deletes an account

--- account.py
+++ account.py
@@ -1,9 +1,10 @@
 class Account:
-    
+    account_list = []
     def __init__(self,user_name,password):
           self.user_name = user_name
           self.password = password
     def save_account(self):
-          Account.account_list = []
           Account.account_list.append(self)
+    def remove_account(self):
+          Account.account_list.remove(self)
      
