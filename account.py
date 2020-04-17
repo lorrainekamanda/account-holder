ca5a88aa09ce20f9bfae619247c2dc8b7df8fdc0
lorrainekamanda <lorrainekamanda@gmail.com>
2020-04-18 00:54:52 +0300

    creating a function that appends/saves my account

--- account.py
+++ account.py
@@ -1,5 +1,9 @@
 class Account:
-    def __init__(self,user_name,password)
-    self.user_name = user_name
-    self.password = password
+    
+    def __init__(self,user_name,password):
+          self.user_name = user_name
+          self.password = password
+    def save_account(self):
+          Account.account_list = []
+          Account.account_list.append(self)
      
