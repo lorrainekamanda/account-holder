82c7fc7dc8b31ecaded9a10a20b948b0d6caa03b
lorrainekamanda <lorrainekamanda@gmail.com>
2020-04-18 02:41:20 +0300

    add class meethod and function that enables one to such for their account

--- account.py
+++ account.py
@@ -7,4 +7,11 @@ class Account:
           Account.account_list.append(self)
     def remove_account(self):
           Account.account_list.remove(self)
+
+    @classmethod
+    def find_by_number(cls,number):
+        for account in cls.account_list:
+            if account.user_name == number:
+                return account
+
      
