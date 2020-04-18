a214e8820de93cb12485e30aa220bbc4ff3a1694
lorrainekamanda <lorrainekamanda@gmail.com>
2020-04-18 12:12:30 +0300

    saving the credentials in an array

--- account.py
+++ account.py
@@ -37,10 +37,23 @@ class Account:
         """
 class Credential:
 
-    def__init__(,self,first_name,last_name,email,phone_number):
+    def __init__(self,first_name,last_name,email,phone_number):
+        """
+        initializing the credential object
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
         self.phone_number = phone_number
 
+    credential_list = []
+    """
+    empty array where credentials will be stored
+    """
+
+    def save_account(self):
+        Credential.credential_list.append(self)
+        """
+        saving credentials
+        """
      
